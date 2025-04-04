51b2913b6b7722c14e175a462c120f3d97266e4d
AntonioJCB <antoniojcalderob@gmail.com>
2025-04-03 23:50:37 -0700

    Update Transferencia.ts

--- src/models/Transferencia.ts
+++ src/models/Transferencia.ts
@@ -1,34 +1,56 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, CreateDateColumn } from "typeorm";
-import { Cuenta } from "./Cuenta";
-
-@Entity()
-export class Transferencia {
-  @PrimaryGeneratedColumn()
-  id!: number;
-
-  @ManyToOne(() => Cuenta, cuenta => cuenta.transferenciasEnviadas)
-  @JoinColumn({ name: "cuentaOrigenId" })
-  cuentaOrigen!: Cuenta;
-
-  @Column()
-  cuentaOrigenId!: number;
-
-  @ManyToOne(() => Cuenta, cuenta => cuenta.transferenciasRecibidas)
-  @JoinColumn({ name: "cuentaDestinoId" })
-  cuentaDestino!: Cuenta;
-
-  @Column()
-  cuentaDestinoId!: number;
-
-  @Column("decimal", { precision: 10, scale: 2 })
-  monto!: number;
-
-  @Column()
-  concepto!: string;
-
-  @CreateDateColumn()
-  fecha!: Date;
-
-  @Column({ default: "Pendiente" })
-  estado!: string;
+export class Transferencia extends Model {
+  public id!: number;
+  public cuentaOrigenId!: number;
+  public cuentaDestinoId!: number;
+  public monto!: number;
+  public concepto!: string;
+  public fecha!: Date;
+  public estado!: string;
 }
+
+Transferencia.init({
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  cuentaOrigenId: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    allowNull: false,
+    references: {
+      model: Cuenta,
+      key: 'id'
+    },
+    field: 'cuenta_origen_id'
+  },
+  cuentaDestinoId: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    allowNull: false,
+    references: {
+      model: Cuenta,
+      key: 'id'
+    },
+    field: 'cuenta_destino_id'
+  },
+  monto: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  concepto: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  fecha: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  estado: {
+    type: DataTypes.ENUM('Pendiente', 'Completada', 'Rechazada'),
+    allowNull: false,
+    defaultValue: 'Pendiente'
+  }
+}, {
+  sequelize,
+  tableName: 'transferencias',
+  timestamps: false
+});
