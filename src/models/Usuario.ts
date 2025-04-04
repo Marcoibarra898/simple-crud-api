aab2d652668044a10194d78b1ba9525a87c0db83
AntonioJCB <antoniojcalderob@gmail.com>
2025-04-03 23:50:57 -0700

    Update Usuario.ts

--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -1,27 +1,41 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn } from "typeorm";
-import "reflect-metadata";
-import { Cuenta } from "./Cuenta";
-
-@Entity()
-export class Usuario {
-  @PrimaryGeneratedColumn()
-  id!: number;
-
-  @Column()
-  nombre!: string;
-
-  @Column()
-  apellido!: string;
-
-  @Column({ unique: true })
-  email!: string;
-
-  @Column()
-  telefono!: string;
-
-  @CreateDateColumn()
-  fechaRegistro!: Date;
-
-  @OneToMany(() => Cuenta, cuenta => cuenta.usuario)
-  cuentas!: Cuenta[];;
+export class Usuario extends Model {
+  public id!: number;
+  public nombre!: string;
+  public apellido!: string;
+  public email!: string;
+  public telefono!: string;
+  public fechaRegistro!: Date;
 }
+
+Usuario.init({
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  nombre: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  apellido: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true
+  },
+  telefono: {
+    type: DataTypes.STRING(20),
+    allowNull: false
+  },
+  fechaRegistro: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  sequelize,
+  tableName: 'usuarios',
+  timestamps: false
+});
